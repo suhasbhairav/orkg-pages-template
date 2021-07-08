169513642a45d3bdb5a9dbb68b52dd0a7d2bc750
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-08 14:19:16 +0200

    Updates

---
+++ precache-manifest.04b4cc76738ce88bca4b461508105b08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccb4fcd5413290d03509b2eb2025fa3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6d55e0716e1801a1414",
+    "url": "./static/css/2.78063165.chunk.css"
+  },
+  {
+    "revision": "b65e19c98f01557d6d53",
+    "url": "./static/css/main.b04fd904.chunk.css"
+  },
+  {
+    "revision": "a6d55e0716e1801a1414",
+    "url": "./static/js/2.0a47c62b.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "./static/js/2.0a47c62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b65e19c98f01557d6d53",
+    "url": "./static/js/main.0fafee89.chunk.js"
+  },
+  {
+    "revision": "0bb4eb37e09b80c79523",
+    "url": "./static/js/runtime-main.7f568536.js"
+  },
+  {
+    "revision": "7d352eef4fc0d24048db63b8a4644f4e",
+    "url": "./static/media/logo.7d352eef.svg"
+  }
+]);
